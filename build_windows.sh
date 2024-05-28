972938922f1ab0f830a278960c11599be70d0e83
riguang <zscboy@gmail.com>
2024-05-28 11:10:31 +0800

    build edge with workerd

--- build_windows.sh
+++ build_windows.sh
@@ -4,5 +4,5 @@ TARGET=gol2.dll
 VERSION='-X github.com/Filecoin-Titan/titan/build.CurrentCommit=+windows'
 LDFLAGS='-extldflags -Wl,-soname,'"$TARGET $VERSION"''
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -ldflags ''"$LDFLAGS $VERSION-$(go env GOARCH)"'' -buildmode=c-shared -o $OUT_PATH/x86_64/$TARGET ./cmd/titan-edge
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build --tags=edge -ldflags ''"$LDFLAGS $VERSION-$(go env GOARCH)"'' -buildmode=c-shared -o $OUT_PATH/x86_64/$TARGET ./cmd/titan-edge
 
