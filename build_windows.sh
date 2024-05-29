12c3d3eecb7ce95dcc3fb64a69b1b99ba44018f0
riguang <zscboy@gmail.com>
2024-05-29 09:43:33 +0800

    build lib for windows

--- build_windows.sh
+++ build_windows.sh
@@ -6,3 +6,5 @@ LDFLAGS='-extldflags -Wl,-soname,'"$TARGET $VERSION"''
 
 GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build --tags=edge -ldflags ''"$LDFLAGS $VERSION-$(go env GOARCH)"'' -buildmode=c-shared -o $OUT_PATH/x86_64/$TARGET ./cmd/titan-edge
 
+## need to set where to find workerd lib
+# export CGO_LDFLAGS="-L/d/lgh-workerd\bazel-bin\src\workerd\server -lgoworkerd"
